646a0a057956d2baa206e75aa6d0668308f11cf0
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2023-03-16 18:04:15 +0530

    AJAX basics

---
+++ asynchronousJs/AJAX_Http_request.js
@@ -0,0 +1,23 @@
+// BASCIC THEORY 
+
+/// AJAX => asynchronous javascript and xml 
+// AJAX stands for Asynchronous JavaScript And XML. In a nutshell, it is the use of the XMLHttpRequest object to communicate with servers. It can send and receive information in various formats, including JSON, XML, HTML, and text files.23-Feb-2023
+
+// HTTP request 
+// HTTP requests are messages sent by the client to initiate an action on the server. Their start-line contain three elements: An HTTP method, a verb (like GET , PUT or POST ) or a noun (like HEAD or OPTIONS ), that describes the action to be performed.
+
+// It is a set of web development techniques 
+// using many web technologies on the client side 
+// to create asynchronous web applications 
+
+// with ajax , web applications can send and retrieve data from a server asynchronously (in the background) without interfering with the display and behaviour of existing web pages 
+// we don't use data in xml format anymore
+// we use json now alot. 
+
+// what is json 
+//It is a text-based way of representing JavaScript object literals, arrays, and scalar data. JSON is relatively easy to read and write, while also easy for software to parse and generate. It is often used for serializing structured data and exchanging it over a network, typically between a server and web applications.
+
+// we have 3 most common ways to create and send requests to server 
+// 1. xmlHTTPRequest (old way of doing)
+// 2. fetch API (new way od doing)
+// 3. axios (this is 3rd party library)
